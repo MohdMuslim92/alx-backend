c38566f8f29b2084c179cb95843eb7469c8b070f
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-01-10 04:40:59 +0300

    Flask app with Babel configuration and translations

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""A Flask app with Babel configuration and translations"""
+
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+
+
+class Config:
+    """Configuration class for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best language for the user"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Render index.html template"""
+    return render_template(
+            '3-index.html',
+            title='Welcome to Holberton',
+            header='Hello world'
+            )
+
+
+if __name__ == '__main__':
+    app.run()
